7c155af54333b9a9d8e147e514706e041d06bbc8
unknown <staub.joao.cs@gmail.com>
2022-01-08 12:11:58 -0300

    1SEGUNDO KKKKKKKKKKKKK

--- 1161.c
+++ 1161.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 
+long int memo[21];
+
+void fat(int a)
+{
+  int i;
+
+  for (i = 2; i <= a; i++)
+    memo[i] = memo[i - 1] * i;
+}
+
 int main()
 {
   int m, n, i;
-  long int r1 = 1, r2 = 1;
+  memo[0] = 1;
+  memo[1] = 1;
+  for (i = 2; i < 21; i++)
+    memo[i] = -1;
   long int soma;
   while (scanf("%d %d", &m, &n))
   {
-    for (i = m; i > 0; i--)
-      r1 *= i;
-    for (i = n; i > 0; i--)
-      r2 *= i;
-
-    printf("%d\n", r1 + r2);
-    r1 = 1;
-    r2 = 1;
+    if (memo[m] == -1 && memo[n] == -1)
+    {
+      if (m >= n)
+        fat(m);
+      else
+        fat(n);
+    }
+    soma = memo[m] + memo[n];
+    printf("%d\n", soma);
   }
-
   return 0;
 }
